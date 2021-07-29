137732d5057f4ea231f116f4ccb8f749870bc440
Vasily Usov <alkal@ya.ru>
2021-07-29 21:54:33 +0300

    Initial Commit

---
+++ SCK_TemplateApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SCK_TemplateApp
+//
+//  Created by Vasily Usov on 29.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
